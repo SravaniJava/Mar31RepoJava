b4fd8eab6b07958783f49b384f790aa6377b9198
sravaniPal <sravanigoudjava.com>
2025-03-31 19:39:10 +0530

    Add changes added in SravaniChild2 by masterBranch

--- src/main/java/org/palni/SravaniChild2.java
+++ src/main/java/org/palni/SravaniChild2.java
@@ -3,5 +3,6 @@ package org.palni;
 public class SravaniChild2 {
     public static void main(String[] args) {
         System.out.println("this is sravaniChild2 added by sravanichild2");
+        System.out.println("this is the sravani child2 added by master");
     }
 }
