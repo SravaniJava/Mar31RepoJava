73a77d8c2fc36cfd45623113b2516684c403cf30
sravaniPal <sravanigoudjava.com>
2025-03-31 18:50:52 +0530

    first repo of the mar31

---
+++ src/main/java/org/palni/Main.java
@@ -0,0 +1,9 @@
+package org.palni;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("first of mar 31 test");
+    }
+}
